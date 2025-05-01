ab0b464980a9f5003531a3419f15decbf7fad0c3
ezmiller-su <ezrajmiller@hotmail.com>
2025-05-01 13:04:20 -0400

    7

--- code/location_dashboard.py
+++ code/location_dashboard.py
@@ -7,18 +7,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 st.set_page_config(layout="wide")
 
-st.title("Top Locations for Parking Within Syracuse")
-st.subheader("Dashboard of parking tickets issued in locations totaling at least $1000 in violation fines.")
 
 tickets_in_top_locations = pd.read_csv("cache/tickets_in_top_locations.csv")
 
-top_locations = pd.read_csv("cache/top_locations.csv")['location']
-location = st.selectbox("Select a location:", top_locations)
+
+st.title("Top Locations for Parking Within Syracuse")
+st.subheader("Dashboard of parking tickets issued in locations totaling at least $1000 in violation fines.")
+
+locations = tickets_in_top_locations['location'].unique()
+
+location = st.selectbox("Select a location:", locations)
+
+
 
 st.text('Total Tickets Issued')
-st.subheader(tickets_in_top_locations[tickets_in_top_locations['location'] == location].shape[0])
-
-
-
+st.metric(tickets_in_top_locations[tickets_in_top_locations['location'] == location].shape[0])
 
 st.text('Total Amount')
+st.metric("Total amount", f"$ {top_locations['amount'].sum()}")
