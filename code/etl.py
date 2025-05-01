d32e8c24ef2c26d162133c50ed173073b30a0f11
ezmiller-su <ezrajmiller@hotmail.com>
2025-04-30 22:40:29 -0400

    3

--- code/etl.py
+++ code/etl.py
@@ -4,13 +4,17 @@ import streamlit as st
 
 def top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     amount_per_location_df = violations_df.pivot_table(index='location', values='amount', aggfunc='sum').sort_values(by='amount')
+    amount_per_location_df['location'] = amount_per_location_df.index
+    amount_per_location_df.reset_index(drop=True, inplace=True)
     amount_per_location_df = amount_per_location_df[amount_per_location_df['amount'] >= threshold]
     return amount_per_location_df
 
 
 def top_locations_mappable(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    violations_df = violations_df[['location', 'lat', 'lon', 'amount']]
-    return pd.DataFrame(violations_df)
+    top_locations_df = top_locations(violations_df, threshold)
+    violation_locations_df = violations_df[['location', 'lat', 'lon']].drop_duplicates(subset=['location'])
+    merged_df = pd.merge(top_locations_df, violation_locations_df, on='location')
+    return merged_df
 
 
 def tickets_in_top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
@@ -20,8 +24,5 @@ if __name__ == '__main__':
     violations_df = pd.read_csv('./cache/final_cuse_parking_violations.csv')
     top_locations_df = top_locations(violations_df)
     top_locations_df.to_csv('./cache/top_locations.csv', index=False)
-    print(top_locations_df.shape)
-    print(violations_df['amount'].max())
-
-
-
+    merged_df = top_locations_mappable(violations_df)
+    merged_df.to_csv('./cache/top_locations_mappable.csv', index=False)
