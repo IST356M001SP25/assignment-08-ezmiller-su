04fc4046588d35ac09c3d4f78ce102ea7f321f41
ezmiller-su <ezrajmiller@hotmail.com>
2025-04-30 21:06:20 -0400

    1

--- code/etl.py
+++ code/etl.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import streamlit as st 
 
-
 def top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    return pd.DataFrame()  # TODO implement this function
+    violations_df = violations_df[['location', 'amount']][violations_df['amount'] > 1000]
+    return violations_df
 
 
 def top_locations_mappable(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
-    return pd.DataFrame()  # TODO implement this function
+    violations_df = violations_df[['location', 'lat', 'lon', 'amount']][violations_df['amount'] > 1000]
+    return pd.DataFrame(violations_df)
 
 
 def tickets_in_top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     return pd.DataFrame()  # TODO implement this function
 
 if __name__ == '__main__':
-    '''
-    Main ETL job. 
-    '''
-    pass # TODO implement this script
+    violations_df = pd.read_csv('./cache/final_cuse_parking_violations.csv')
+    top_locations_df = top_locations(violations_df)
+    print(top_locations_df)
